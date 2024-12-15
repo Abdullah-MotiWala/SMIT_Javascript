fc96e21baf52aaf8358720af48fc006a67327992
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-12-15 20:51:08 +0500

    initial

---
+++ app.js
@@ -0,0 +1,28 @@
+// primitives
+
+var type1 = 1
+var type2 = true
+var type3 = "ahmed";
+var type4 = undefined
+var type5 = null
+
+var camelCaseOne = "ahmed";
+var snake_case = "ahmed";
+var PascalCase = "ahmed";
+var KEBAB_CASE = "ahmed";
+
+var _value = ""
+
+
+// alert(undefined)
+// console.log(true)
+
+console.log(newValue)
+// var newValue; // decelare
+var newValue = 1;
+
+// newValue = "assign";
+// console.log(newValue)
+
+
+
