56094714c6e2d1150daed3341d0c4c2cb48eb05d
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-12-21 20:49:30 +0500

    if else started

--- app.js
+++ app.js
@@ -11,18 +11,117 @@ var snake_case = "ahmed";
 var PascalCase = "ahmed";
 var KEBAB_CASE = "ahmed";
 
+// Non Premitive
+var myArray = ["ahmed", "yasin", "raees", false, 45, null, undefined, [1, 2], { poition: "first", name: "Ahmed" }]
+
+var myObject = { name: "Ahmed", age: 22, married: false, children: ["taha", "gul khan"] }
+
 var _value = ""
 
 
 // alert(undefined)
 // console.log(true)
-
-console.log(newValue)
+// console.log("first")
+// console.log(newValue)
+// var newValue = 1;
+// console.log(newValue)
+// console.log("second")
 // var newValue; // decelare
-var newValue = 1;
 
 // newValue = "assign";
 // console.log(newValue)
+
+
+
+// var firstName = "Abdulla'h"
+// var firstName2 = 'Abdulla"h'
+
+// console.log(typeof firstName)
+
+var firstNum = 4
+var secondNum = 10
+
+// firstNum = firstNum + firstNum
+// console.log(firstNum + 5)
+// console.log(firstNum, secondNum)
+
+// firstNum = firstNum + firstNum
+// console.log(firstNum)
+// console.log(secondNum)
+
+// var subtraction = firstNum - secondNum
+// console.log(subtraction, "subtraction")
+
+// var multiplication = firstNum * secondNum
+// console.log(multiplication, "multiplication")
+
+// var division = firstNum / secondNum
+// console.log(division, "division")
+
+// var remainder = secondNum % firstNum
+// console.log(remainder, "remainder")
+
+// firstNum = firstNum + 5
+// firstNum += 5
+// console.log(firstNum)
+
+// firstNum = firstNum + 1
+// firstNum += 1
+
+
+// firstNum++
+// console.log(firstNum)
+// console.log(firstNum++)
+// console.log(firstNum)
+
+// console.log(++firstNum)
+// console.log(firstNum--)
+// console.log(--firstNum)
+
+// BODMAS
+// var result = 2 + (22 + 6) / 4 * 2
+// console.log(result)
+
+
+// var a = 1 + 1
+// var b = "one" + "one"
+// console.clear()
+// console.log(a, b)
+
+// console.clear()
+// var a = 2 - "1"
+// console.log(a)
+// var age = 26
+// var userAge = prompt("Enter Your Age","26")
+// console.log(typeof userAge)
+
+// var age = prompt("Enter Your Age")
+// var addedAge = age + 2
+// console.log(addedAge)
+
+
+// var a = 1
+// var b = 2
+
+// console.log(a != b)
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a >= b)
+
+// var role = prompt("Enter your role")
+
+// var teacherRole = "teacher"
+// var studentRole = "student"
+
+// if (role === teacherRole) {
+//     console.clear()
+//     console.log("Password : abcdef")
+// } else if (role === studentRole) {
+//     console.log("Password : ghkie")
+// } else {
+//     console.log("Access Denied")
+// }
 
 
 
